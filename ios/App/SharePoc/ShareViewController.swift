e89484b27e4ad1a605a4e4d89b1fd420ef0d1e4d
ibrahima0404 <ibkhgueye@gmail.com>
2021-09-07 17:16:54 +0200

    add custom view when images are loading

--- ios/App/SharePoc/ShareViewController.swift
+++ ios/App/SharePoc/ShareViewController.swift
@@ -16,15 +16,50 @@ class ShareViewController: SLComposeServiceViewController, ShareViewControllerDe
     
     private var imageString: String?
     private var fileString: String?
-    var delegate: ShareViewControllerDelegate?
+    private var delegate: ShareViewControllerDelegate?
+    private let messageLoadingSeveralPhotos = "Chargement de %@ vos photos"
+    private let messageLoadingOnePhoto = "Chargement de votre photo"
     
     private var appURL = "sar.poc.sesame.ios.ionic://?text="
-   
+    
+    let spinner = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+    let infoLabel = UILabel()
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        setupLoadingView()
         handleSharedImages()
     }
-
+    
+    private func setupLoadingView() {
+        infoLabel.translatesAutoresizingMaskIntoConstraints = false
+        infoLabel.textAlignment = .center
+        infoLabel.textColor = .white
+        
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        
+        let customView = UIView(frame: view.frame)
+        customView.backgroundColor = .red
+        
+        customView.addSubview(spinner)
+        customView.addSubview(infoLabel)
+        view.addSubview(customView)
+        spinner.centerXAnchor.constraint(equalTo: customView.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: customView.centerYAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            infoLabel.bottomAnchor.constraint(equalTo: spinner.topAnchor, constant: -20),
+            infoLabel.centerXAnchor.constraint(equalTo: customView.centerXAnchor),
+            infoLabel.heightAnchor.constraint(equalToConstant: 40)
+            ])
+    }
+    
+    private func setupLoadingMessage(with numberOfPhoto: Int) {
+        DispatchQueue.main.async {
+            self.infoLabel.text = numberOfPhoto > 1 ? String(format: self.messageLoadingSeveralPhotos, "\(numberOfPhoto)") : self.messageLoadingOnePhoto
+        }
+    }
+    
     private func openMainApp() {
         appURL = appURL + "&url=" + appURL
         appURL = appURL + "&image=" + (self.imageString ?? "")
@@ -55,6 +90,7 @@ class ShareViewController: SLComposeServiceViewController, ShareViewControllerDe
         let contentType = kUTTypeData as String
         var strBase64String = ""
         
+        setupLoadingMessage(with: attachments.count)
         for index in 0..<attachments.count {
             let provider = attachments[index]
             if provider.hasItemConformingToTypeIdentifier(contentType) {
@@ -78,6 +114,7 @@ class ShareViewController: SLComposeServiceViewController, ShareViewControllerDe
                         if index == (attachments.count - 1) {
                             self.imageString = String(strBase64String.dropLast())
                             self.delegate = self
+                            self.spinner.stopAnimating()
                             delegate?.didFinishLoadImage(isImagesLoaded: true)
                         }
                     }else {
